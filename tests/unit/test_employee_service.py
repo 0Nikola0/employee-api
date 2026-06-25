48a98286a96d62f4ac584f69a84ee81aadf9dec4
0Nikola0 <dev0nikola0@gmail.com>
2026-06-25 23:19:18 +0200

    fix mypy error

--- tests/unit/test_employee_service.py
+++ tests/unit/test_employee_service.py
@@ -1,8 +1,10 @@
+from typing import cast
 from datetime import date
 
 import pytest
 
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
 from models.db import Employee
 from service.employee_service import EmployeeService
@@ -35,7 +37,7 @@ def test_convert_employees_to_csv_returns_header_and_row() -> None:
 
 
 def test_get_employee_by_id_raises_404_when_missing() -> None:
-    service = EmployeeService(db=object())
+    service = EmployeeService(db=cast(Session, object()))
     service.employee_repo = type("Repo", (), {"get_by_id": lambda self, _: None})()
 
     with pytest.raises(HTTPException) as exc_info:
